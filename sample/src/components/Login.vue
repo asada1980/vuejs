0b12593fb28e35f3e879f2adb927b2d0026f3e85
asada <kenichi.asada.g@gmail.com>
2019-04-14 18:34:10 +0900

    初回

---
+++ sample/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template src="./Login.html" />
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style scoped src="./HelloWorld.scss"/> -->
